a43abc486aa14f8030c6adb97d6fdad61c8a9b67
Cassidy Fortner <cassidy.fortner@gmail.com>
2020-03-06 17:09:24 -0600

    setting up heroku connection

--- server.js
+++ server.js
@@ -11,14 +11,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+//connecting to database for deployed
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
+mongoose.connect(MONGODB_URI)
 
 let api_routes = require("./routes/api-routes");
 let html_routes = require("./routes/html-routes");
 app.use(api_routes);
 app.use(html_routes);
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
